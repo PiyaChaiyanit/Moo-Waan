973f85571ce2957538ca4aea6e97853f05c7ac85
JastKherZ <jastkherz@hotmail.com>
2023-03-01 23:56:43 +0700

    Finish EditItemPage

---
+++ src/components/EditItemPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <p class="is-size-1">Edit Item</p>
+        <div class="box has-background-primary">
+            <p class="is-size-5">Select Item</p>
+            <input class="input" type="select_use"><br><br>
+            <input class="input is-rounded is-large" type="select_item"><br><br>
+            <div class="columns">
+                <div class="column is-half">
+                    <p>
+                        Amount:
+                    </p>
+                    <input class="input" type="text">
+                </div>
+                <div class="column is-half">
+                    <p>Price:</p>
+                    <input class="input" type="text">
+                </div>
+            </div>
+            <button class="button is-rounded is-warning">Update</button>
+        </div>
+    </div>
+</template>
+    
+<script>
+export default {
+    name: 'EditItemPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+    
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
+    
