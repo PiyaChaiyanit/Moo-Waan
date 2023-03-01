33287c558c7535150dc8e3fec2d13e666d98c9e9
JastKherZ <jastkherz@hotmail.com>
2023-03-01 23:05:41 +0700

    finish EditUserPage

---
+++ src/components/EditUserPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="block is-size-1">
+        Edit User
+        <div class="container">
+                <div class="box is-size-6 has-background-info">
+                    SELECT USER
+                    <div class="column">
+                        <input class="input is-rounded mx-1" type="text"><br><br>
+                        <input class="input is-large is-rounded mx-1" type="text"><br><br>
+                        <div class="button is-rounded">
+                            DELETE
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'EditUserPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,49 +1,53 @@
 <template>
-    <div class="block">
-      <AdminNav />
-    </div>
-  
-    <div class="home">
-      <HomePage />
-      <!-- <img alt="Vue logo" src="../assets/logo.png">
+  <div class="block">
+    <AdminNav />
+  </div>
+
+  <div class="home">
+    <HomePage />
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    </div>
-  
-    <div class="block">
-      <ItemCard />
-    </div>
-  
-    
-  
-    <div class="block">
-      <FooterPage />
-    </div>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-  // import HelloWorld from '@/components/HelloWorld.vue'
-  import AdminNav from '@/components/AdminNav.vue';
-  import HomePage from '@/components/HomePage.vue';
-  import ItemCard from '@/components/ItemCard.vue';
-  import FooterPage from '@/components/FooterPage.vue';
+  </div>
 
-  
-  
-  export default {
-    name: 'HomeView',
-    components: {
-      // HelloWorld,
-      AdminNav,
-      HomePage,
-      ItemCard,
-      FooterPage,
+  <div class="block">
+    <ItemCard />
+  </div>
 
-  }
-  }
-  </script>
+  <div class="block">
+    <EditUserPage />
+  </div>
+
+
+  <div class="block">
+    <FooterPage />
+  </div>
+</template>
   
-  <style>
-    @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
-  </style>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import AdminNav from '@/components/AdminNav.vue';
+import HomePage from '@/components/HomePage.vue';
+import ItemCard from '@/components/ItemCard.vue';
+import FooterPage from '@/components/FooterPage.vue';
+import EditUserPage from '@/components/EditUserPage.vue';
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld,
+    AdminNav,
+    HomePage,
+    ItemCard,
+    FooterPage,
+    EditUserPage
+  }
+}
+</script>
+  
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
   
