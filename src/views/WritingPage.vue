3ffbe5d6d0e7a1f6f7324c5d61f3fd0634b8b024
PiyaChaiyanit <64070186@kmitl.ac.th>
2023-03-02 15:24:16 +0700

    add item.json to deco,tool,paper,writer

--- src/views/WritingPage.vue
+++ src/views/WritingPage.vue
@@ -7,78 +7,37 @@
         Writing Item
     </div>
 
-    <div class="tile is-ancestor">
-        <div class="tile is-parent">
-            <article class="tile is-child box">
-                <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-                <br>
-                <p class="is-size-5">gay</p>
-                <br>
-                <p class="has-text-left">ปากกาสีน้ำเงิน</p>
-                <p class="has-text-left">ราคา : 50 ฿</p>
-                <br>
-                <div class="has-text-right">
-                    <router-link to="/SelectedItems" class="button is-rounded">
-                        Select
-                    </router-link>
+    <div class="block container">
+        <div class="is-multiline columns">
+
+            <!-- Card element start here------------------------------------------>
+
+            <div class="column is-one-quarter" v-for="(todo, index) in item" :key="index">
+                <div class="card" v-if="todo.type === 'write'">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src="https://bulma.io/images/placeholders/640x480.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">{{ todo.name }}</p>
+                                <p class="has-text-left">ชนิด : {{ todo.type }}</p>
+                                <p class="has-text-left">ราคา : {{ todo.price }} ฿</p>
+                            </div>
+                        </div>
+                        <div class="has-text-right">
+                            <router-link to="/SelectedItems" class="button is-rounded">
+                                Select
+                            </router-link>
+                        </div>
+
+                    </div>
                 </div>
-            </article>
-        </div>
-        <div class="tile is-parent">
-            <article class="tile is-child box">
-                <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-                <br>
-                <p class="is-size-5">gay</p>
-                <br>
-                <p class="has-text-left">ปากกาสีน้ำเงิน</p>
-                <p class="has-text-left">ราคา : 50 ฿</p>
-                <br>
-                <div class="has-text-right">
-                    <router-link to="/SelectedItems" class="button is-rounded">
-                        Select
-                    </router-link>
-                </div>
-            </article>
-        </div>
-        <div class="tile is-parent">
-            <article class="tile is-child box">
-                <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-                <br>
-                <p class="is-size-5">gay</p>
-                <br>
-                <p class="has-text-left">ปากกาสีน้ำเงิน</p>
-                <p class="has-text-left">ราคา : 50 ฿</p>
-                <br>
-                <div class="has-text-right">
-                    <router-link to="/SelectedItems" class="button is-rounded">
-                        Select
-                    </router-link>
-                </div>
-            </article>
-        </div>
-        <div class="tile is-parent">
-            <article class="tile is-child box">
-                <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-                <br>
-                <p class="is-size-5">gay</p>
-                <br>
-                <p class="has-text-left">ปากกาสีน้ำเงิน</p>
-                <p class="has-text-left">ราคา : 50 ฿</p>
-                <br>
-                <div class="has-text-right">
-                    <router-link to="/SelectedItems" class="button is-rounded">
-                        Select
-                    </router-link>
-                </div>
-            </article>
+            </div>
+
+
         </div>
     </div>
 
@@ -88,6 +47,8 @@
 </template>
 
 <script>
+import item from "../item.json"
+
 import NavBar from '@/components/NavBar.vue';
 import FooterPage from '@/components/FooterPage.vue';
 
@@ -98,10 +59,14 @@ export default {
         msg: String
     },
     components: {
-    NavBar,
-    FooterPage,
-    
-}
+        NavBar,
+        FooterPage,
+    },
+    data() {
+        return {
+            item
+        }
+    }
 }
 </script>
 
