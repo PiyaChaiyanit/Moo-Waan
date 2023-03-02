f6eb64a69129c229db7cf72d7cb1f4b11aa5cc37
JastKherZ <jastkherz@hotmail.com>
2023-03-02 16:38:45 +0700

    Finish HistoryPage

---
+++ src/components/HistoryPage.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <div class="box">
+            <p class="has-text-left m-2 is-size-2">Order History</p>
+            <div class="columns">
+                <div class="input">
+                    <div class="column">
+                        <p>Order</p>
+                    </div>
+                    <div class="column">
+                        <p>Placed</p>
+                    </div>
+                    <div class="column">
+                        <p>Name</p>
+                    </div>
+                    <div class="column">
+                        <p>Status</p>
+                    </div>
+                    <div class="column">
+                        <p>Cost</p>
+                    </div>
+                    <div class="column">
+                        <p>Check</p>
+                    </div>
+                </div>
+            </div>
+            <div class="input">
+                <div class="column">
+                    <p>1</p>
+                </div>
+                <div class="column">
+                    <p>23/2/2566</p>
+                </div>
+                <div class="column">
+                    <p>Piya</p>
+                </div>
+                <div class="column">
+                    <p>Not Comlplete</p>
+                </div>
+                <div class="column">
+                    <p>50 ฿</p>
+                </div>
+                <div class="column">
+                    <div class="column">
+                        <div class="button">
+                            ❌
+                        </div>
+                        <div class="button">
+                            ✔️
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HistoryPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,9 @@
     <ItemCard />
   </div>
 
+  <div class="block">
+    <HistoryPage />
+  </div>
 
   <div class="block">
     <FooterPage />
@@ -26,6 +29,7 @@ import NavBar from '@/components/NavBar.vue';
 import HomePage from '@/components/HomePage.vue';
 import ItemCard from '@/components/ItemCard.vue';
 import FooterPage from '@/components/FooterPage.vue';
+import HistoryPage from '@/components/HistoryPage.vue';
 
 
 
@@ -37,6 +41,7 @@ export default {
     HomePage,
     ItemCard,
     FooterPage,
+    HistoryPage
     
 }
 }
