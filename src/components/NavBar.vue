b0292eaa441ccc0945bd55c9657a7255dd740685
JastKherZ <jastkherz@hotmail.com>
2023-03-03 09:27:10 +0700

    dasssssssss

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
-      <h1>Sunny Shop ไก่</h1>
+      <h1>Moo-Waan Shop</h1>
     </a>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
